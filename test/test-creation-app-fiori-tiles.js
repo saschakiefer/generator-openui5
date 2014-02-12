47d8451c1999aec9d02be902dab63fb73d257ee6
Jason Scott <jason@jaylin.com.au>
2014-02-12 10:18:24 +0800

    test(tiles app): Added extra tests for index.html scaffolding
    Now testing for correct templating of UI5 bootstrap location, Component namespace and title in index.html.
    Also removed extra whitespace from _index.html.
    
    Based-on: #45

--- test/test-creation-app-fiori-tiles.js
+++ test/test-creation-app-fiori-tiles.js
@@ -5,10 +5,12 @@
 	var path = require("path");
 	var helpers = require("yeoman-generator").test;
 	var assert = require("assert");
+	var chalk = require("chalk");
+	var cheerio = require("cheerio");
 
 	describe("openui5 app generator - fiori tiles", function() {
 		beforeEach(function(done) {
-			console.log("*** Fiori Tiles App ***");
+			console.log(chalk.blue("*** Fiori Tiles App ***"));
 
 			helpers.testDirectory(path.join(__dirname, "temp"), function(err) {
 				if (err) {
@@ -69,8 +71,8 @@
 			});
 		});
 
-		it("templates index.html properly", function(done) {
-			console.log("*** CHECK TILES APP INDEX.HTML SCAFFOLDING ***");
+		it("templates index.html properly - with bower UI5 library", function(done) {
+			console.log(chalk.blue("*** CHECK TILES APP INDEX.HTML SCAFFOLDING - BOWER ***"));
 
 			var mockPrompts = {
 				applicationName: "My Tiles Application",
@@ -88,14 +90,47 @@
 			this.app.options["skip-install"] = true;
 			
 			this.app.run({}, function() {
-				var cheerio = require("cheerio"),
-					indexFile = this.app.readFileAsString("index.html"),
-					$ = cheerio.load(indexFile);
+				var indexFile = this.app.readFileAsString("index.html"),
+					$ = cheerio.load(indexFile),
+					resourceRoots = JSON.parse($("#sap-ui-bootstrap").attr("data-sap-ui-resourceroots"));
 
 				assert($("title").text() === "My Tiles Application", "index.html title is incorrect");
 				assert($("#sap-ui-bootstrap").attr("src") === "bower_components/openui5-bower/resources/sap-ui-core.js",
 							"index.html bootstrap tag is incorrect. Should be " + "bower_components/openui5-bower/resources/sap-ui-core.js");
-				//TODO: assert($("script"));
+				assert(resourceRoots["sap.ui.demo"], "Component namespace invalid. Should be: sap.ui.demo");
+
+				done();
+			}.bind(this));
+		});
+
+		it("templates index.html properly - with custom UI5 library location", function(done) {
+			console.log(chalk.blue("*** CHECK TILES APP INDEX.HTML SCAFFOLDING - CUSTOM ***"));
+
+			var mockPrompts = {
+				applicationName: "My Tiles Application",
+				appDescription: "Test Description",
+				authorName: "John Doe",
+				gitRepository: "ssh://github.com/ropository/url.git",
+				licenseType: "Apache License, Version 2.0",
+				applicationType: "tiles",
+				fioriComponentNamespace: "sap.ui.demo",
+				openUI5LocationOption: "custom",
+				openUI5Location: "https://openui5.netweaver.ondemand.com/resources"
+			};
+
+			helpers.mockPrompt(this.app, mockPrompts);
+			this.app.args = ["Main", false];
+			this.app.options["skip-install"] = true;
+			
+			this.app.run({}, function() {
+				var indexFile = this.app.readFileAsString("index.html"),
+					$ = cheerio.load(indexFile),
+					resourceRoots = JSON.parse($("#sap-ui-bootstrap").attr("data-sap-ui-resourceroots"));
+
+				assert($("title").text() === "My Tiles Application", "index.html title is incorrect");
+				assert($("#sap-ui-bootstrap").attr("src") === "https://openui5.netweaver.ondemand.com/resources/sap-ui-core.js",
+							"index.html bootstrap tag is incorrect. Should be " + "https://openui5.netweaver.ondemand.com/resources/sap-ui-core.js");
+				assert(resourceRoots["sap.ui.demo"], "Component namespace invalid. Should be: sap.ui.demo");
 
 				done();
 			}.bind(this));
